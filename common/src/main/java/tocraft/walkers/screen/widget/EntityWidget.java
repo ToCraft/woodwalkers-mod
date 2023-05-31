d63f617eac53eaada015f1fdc0323f3f2ae04fec
ToCraft <tobias.cuiper@gmx.de>
2023-05-31 22:16:16 +0200

    fixed small render bug

--- common/src/main/java/tocraft/walkers/screen/widget/EntityWidget.java
+++ common/src/main/java/tocraft/walkers/screen/widget/EntityWidget.java
@@ -3,9 +3,16 @@ package tocraft.walkers.screen.widget;
 import tocraft.walkers.api.variant.ShapeType;
 import tocraft.walkers.network.impl.SwapPackets;
 import tocraft.walkers.screen.WalkersScreen;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.ingame.InventoryScreen;
 import net.minecraft.client.gui.screen.narration.NarrationMessageBuilder;
 import net.minecraft.client.gui.widget.PressableWidget;
+import net.minecraft.client.render.DiffuseLighting;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.EntityRenderDispatcher;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.text.Text;
@@ -16,6 +23,7 @@ public class EntityWidget<T extends LivingEntity> extends PressableWidget {
     private final T entity;
     private final int size;
     private final WalkersScreen parent;
+    private boolean crashed;
 
     public EntityWidget(float x, float y, float width, float height, ShapeType<T> type, T entity, WalkersScreen parent) {
         super((int) x, (int) y, (int) width, (int) height, Text.of("")); // int x, int y, int width, int height, message
@@ -40,12 +48,20 @@ public class EntityWidget<T extends LivingEntity> extends PressableWidget {
     public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
         super.render(matrices, mouseX, mouseY, delta);
 
-        // Some entities (namely Aether mobs) crash when rendered in a GUI.
-        // Unsure as to the cause, but this try/catch should prevent the game from entirely dipping out.
-        try {
-            InventoryScreen.drawEntity(matrices, this.getX() + this.getWidth() / 2, (int) (this.getY() + this.getHeight() * .75f), size, -10, -10, entity);
-        } catch (Exception ignored) {
-
+        if(!crashed) {
+            // Some entities (namely Aether mobs) crash when rendered in a GUI.
+            // Unsure as to the cause, but this try/catch should prevent the game from entirely dipping out.
+            try {
+                InventoryScreen.drawEntity(matrices, this.getX() + this.getWidth() / 2, (int) (this.getY() + this.getHeight() * .75f), size, -10, -10, entity);
+            } catch (Exception ignored) {
+                crashed = true;
+                VertexConsumerProvider.Immediate immediate = MinecraftClient.getInstance().getBufferBuilders().getEntityVertexConsumers();
+                immediate.draw();
+                EntityRenderDispatcher entityRenderDispatcher = MinecraftClient.getInstance().getEntityRenderDispatcher();
+                entityRenderDispatcher.setRenderShadows(true);
+                RenderSystem.getModelViewStack().pop();
+                DiffuseLighting.enableGuiDepthLighting();
+            }
         }
     }
 
