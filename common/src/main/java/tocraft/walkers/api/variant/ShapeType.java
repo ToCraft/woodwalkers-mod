5298172dd4edecea3e727c1d764c4b6771a0f7b9
ToCraft <tobias.cuiper@gmx.de>
2023-07-25 09:31:16 +0200

    improved DevWolfCheck

--- common/src/main/java/tocraft/walkers/api/variant/ShapeType.java
+++ common/src/main/java/tocraft/walkers/api/variant/ShapeType.java
@@ -99,7 +99,7 @@ public class ShapeType<T extends LivingEntity> {
 
         List<ShapeType<?>> types = new ArrayList<>();
         for (EntityType<?> type : LIVING_TYPE_CASH) {
-            if((type != Registries.ENTITY_TYPE.get(new Identifier("minecraft:wolf")) || (!SyncedVars.getShapeBlacklist().isEmpty() || SyncedVars.getShapeBlacklist().contains(EntityType.getId(type).toString()))) && VARIANT_BY_TYPE.containsKey(type)) {
+            if((type != EntityType.WOLF || (!SyncedVars.getShapeBlacklist().isEmpty() || SyncedVars.getShapeBlacklist().contains(EntityType.getId(type).toString()))) && VARIANT_BY_TYPE.containsKey(type)) {
                 TypeProvider<?> variant = VARIANT_BY_TYPE.get(type);
                 for (int i = 0; i <= variant.getRange(); i++) {
                     types.add(new ShapeType(type, i));
