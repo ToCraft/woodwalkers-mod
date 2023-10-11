ecedd64163863b08714bd3b92ea2b564f44ace1f
ToCraft <tobias.cuiper@gmx.de>
2023-10-11 08:35:10 +0200

    This implements #24

--- common/src/main/java/tocraft/walkers/registry/WalkersEventHandlers.java
+++ common/src/main/java/tocraft/walkers/registry/WalkersEventHandlers.java
@@ -3,17 +3,21 @@ package tocraft.walkers.registry;
 import dev.architectury.event.EventResult;
 import dev.architectury.event.events.common.InteractionEvent;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.animal.horse.AbstractHorse;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.monster.Ravager;
+import net.minecraft.world.entity.player.Player;
 import tocraft.walkers.Walkers;
 import tocraft.walkers.api.PlayerHostility;
 import tocraft.walkers.api.PlayerShape;
+import tocraft.walkers.impl.PlayerDataProvider;
 
 public class WalkersEventHandlers {
 
 	public static void initialize() {
 		WalkersEventHandlers.registerHostilityUpdateHandler();
 		WalkersEventHandlers.registerRavagerRidingHandler();
+		WalkersEventHandlers.registerPlayerRidingHandler();
 	}
 
 	public static void registerHostilityUpdateHandler() {
@@ -31,7 +35,8 @@ public class WalkersEventHandlers {
 	// be able to ride Ravagers.
 	public static void registerRavagerRidingHandler() {
 		InteractionEvent.INTERACT_ENTITY.register((player, entity, hand) -> {
-			if (entity instanceof Ravager) {
+			// checks, if selected entity is a Ravager or a Player, shaped as a Rvager
+			if (entity instanceof Ravager || entity instanceof Player targetedPlayer && ((PlayerDataProvider) targetedPlayer).getCurrentShape() instanceof Ravager) {
 				LivingEntity shape = PlayerShape.getCurrentShape(player);
 				if (shape != null) {
 					if (shape.getType().is(WalkersEntityTags.RAVAGER_RIDING)) {
@@ -40,6 +45,18 @@ public class WalkersEventHandlers {
 				}
 			}
 
+			return EventResult.pass();
+		});
+	}
+	
+	// make this server-side
+	public static void registerPlayerRidingHandler() {
+		InteractionEvent.INTERACT_ENTITY.register((player, entity, hand) -> {
+			if (entity instanceof Player playerToBeRidden) {
+				if (((PlayerDataProvider) playerToBeRidden).getCurrentShape() instanceof AbstractHorse) {
+					player.startRiding(playerToBeRidden, true);
+				}
+			}
 			return EventResult.pass();
 		});
 	}
