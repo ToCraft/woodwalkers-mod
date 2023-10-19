815fe226e84cd08a59fb6f7fd2dc5c57ee755675
ToCraft <tobias.cuiper@gmx.de>
2023-10-19 11:03:47 +0200

    moved publishing to subprojects

--- autoupload.bat
+++ autoupload.bat
@@ -11,11 +11,11 @@ for %%m in (%mcversions%) DO (
     git checkout "%%m"
     gh release create "%version%-%%m" --generate-notes
 
+    .\gradlew publish
     for %%l in (%modloader%) DO (
         call .\gradlew %%l:build
         .\gradlew %%l:modrinth
         .\gradlew %%l:curseforge
-        .\gradlew %%l:publish
         move "%%l\build\libs\walkers-%version%-%%l.jar" "%%l\build\walkers-%%m-%%l-%version%.jar"
         rmdir /s /q "%%l\build\libs"
         gh release upload "%version%-%%m" "%%l\build\walkers-%%m-%%l-%version%.jar"
