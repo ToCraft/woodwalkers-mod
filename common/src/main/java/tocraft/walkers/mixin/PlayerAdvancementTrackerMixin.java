bf7c9140219fc4c9eb873db3c667650bee199acb
ToCraft <tobias.cuiper@gmx.de>
2023-10-22 15:36:21 +0200

    fix bug for 1.20.2

--- common/src/main/java/tocraft/walkers/mixin/PlayerAdvancementTrackerMixin.java
+++ common/src/main/java/tocraft/walkers/mixin/PlayerAdvancementTrackerMixin.java
@@ -6,7 +6,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import net.minecraft.advancements.Advancement;
+import net.minecraft.advancements.AdvancementHolder;
 import net.minecraft.server.PlayerAdvancements;
 import net.minecraft.server.level.ServerPlayer;
 import tocraft.walkers.Walkers;
@@ -21,7 +21,7 @@ public class PlayerAdvancementTrackerMixin {
             method = "award",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/advancements/AdvancementRewards;grant(Lnet/minecraft/server/level/ServerPlayer;)V")
     )
-    private void refreshFlight(Advancement advancement, String criterionName, CallbackInfoReturnable<Boolean> cir) {
+    private void refreshFlight(AdvancementHolder advancement, String criterionName, CallbackInfoReturnable<Boolean> cir) {
         if(Walkers.hasFlyingPermissions(player)) {
             FlightHelper.grantFlightTo(player);
             player.getAbilities().setFlyingSpeed(Walkers.CONFIG.flySpeed);
