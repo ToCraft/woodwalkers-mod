f896ce781db1fd3de777cbf3271bd24d4fb9d423
ToCraft <piminecraft492@gmail.com>
2023-12-23 14:34:31 +0100

    fix wrong hitbox after death

--- common/src/main/java/tocraft/walkers/mixin/player/RespawnDataCopyMixin.java
+++ common/src/main/java/tocraft/walkers/mixin/player/RespawnDataCopyMixin.java
@@ -6,6 +6,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import tocraft.walkers.api.PlayerShapeChanger;
+import tocraft.walkers.impl.DimensionsRefresher;
 import tocraft.walkers.impl.PlayerDataProvider;
 
 @Mixin(ServerPlayer.class)
@@ -21,6 +22,7 @@ public class RespawnDataCopyMixin {
         newData.walkers$setRemainingHostilityTime(oldData.walkers$getRemainingHostilityTime());
         newData.walkers$setCurrentShape(oldData.walkers$getCurrentShape());
         newData.walkers$set2ndShape(oldData.walkers$get2ndShape());
+        ((DimensionsRefresher) (ServerPlayer) (Object) this).shape_refreshDimensions();
 
         PlayerShapeChanger.sync((ServerPlayer) (Object) this);
     }
