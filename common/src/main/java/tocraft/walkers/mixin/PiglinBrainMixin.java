da9a7a8f3f9d516b3340fb18babf650e41e30e96
ToCraft <piminecraft492@gmail.com>
2023-12-23 15:41:00 +0100

    fix Piglin AI errors

--- common/src/main/java/tocraft/walkers/mixin/PiglinBrainMixin.java
+++ common/src/main/java/tocraft/walkers/mixin/PiglinBrainMixin.java
@@ -1,14 +1,14 @@
 package tocraft.walkers.mixin;
 
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.monster.Enemy;
-import net.minecraft.world.entity.monster.piglin.Piglin;
-import net.minecraft.world.entity.monster.piglin.PiglinAi;
-import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.monster.Enemy;
+import net.minecraft.world.entity.monster.piglin.PiglinAi;
+import net.minecraft.world.entity.player.Player;
 import tocraft.walkers.Walkers;
 import tocraft.walkers.api.PlayerHostility;
 import tocraft.walkers.api.PlayerShape;
@@ -16,20 +16,19 @@ import tocraft.walkers.registry.WalkersEntityTags;
 
 @Mixin(PiglinAi.class)
 public class PiglinBrainMixin {
-
-	@Inject(method = "isNearestValidAttackTarget", at = @At("RETURN"), cancellable = true)
-	private static void shouldAttackShape(Piglin piglin, LivingEntity target, CallbackInfoReturnable<Boolean> cir) {
-		boolean shouldAttack = cir.getReturnValue();
-
-		if (shouldAttack && target instanceof Player player) {
+	
+	@Inject(method = "isWearingGold", at = @At("RETURN"), cancellable = true)
+	private static void shapeIsWearingGold(LivingEntity livingEntity, CallbackInfoReturnable<Boolean> cir) {
+		boolean wearingGold = cir.getReturnValue();
+		
+		if (!wearingGold && livingEntity instanceof Player player) {
 			LivingEntity shape = PlayerShape.getCurrentShape(player);
-			boolean hasHostility = PlayerHostility.hasHostility(player);
 
 			if (shape != null) {
 				// Piglins should not attack Piglins or Piglin Brutes, unless they have
 				// hostility
 				if (shape.getType().is(WalkersEntityTags.PIGLIN_FRIENDLY)) {
-					cir.setReturnValue(false);
+					cir.setReturnValue(true);
 				}
 
 				// Player has a shape but is not a piglin, check config for what to do
@@ -37,9 +36,7 @@ public class PiglinBrainMixin {
 					if (Walkers.CONFIG.hostilesIgnoreHostileShapedPlayer && shape instanceof Enemy) {
 
 						// Check hostility for aggro on non-piglin hostiles
-						if (!hasHostility) {
-							cir.setReturnValue(false);
-						} else {
+						if (PlayerHostility.hasHostility(player)) {
 							cir.setReturnValue(true);
 						}
 					}
--- common/src/main/java/tocraft/walkers/mixin/PiglinBruteBrainMixin.java
+++ common/src/main/java/tocraft/walkers/mixin/PiglinBruteBrainMixin.java
@@ -1,34 +1,52 @@
 package tocraft.walkers.mixin;
 
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.boss.wither.WitherBoss;
-import net.minecraft.world.entity.monster.WitherSkeleton;
-import net.minecraft.world.entity.monster.piglin.AbstractPiglin;
-import net.minecraft.world.entity.monster.piglin.PiglinBruteAi;
-import net.minecraft.world.entity.player.Player;
+import java.util.Optional;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.memory.MemoryModuleType;
+import net.minecraft.world.entity.monster.Enemy;
+import net.minecraft.world.entity.monster.piglin.AbstractPiglin;
+import net.minecraft.world.entity.monster.piglin.PiglinBruteAi;
+import net.minecraft.world.entity.player.Player;
+import tocraft.walkers.Walkers;
+import tocraft.walkers.api.PlayerHostility;
 import tocraft.walkers.api.PlayerShape;
+import tocraft.walkers.registry.WalkersEntityTags;
 
 @Mixin(PiglinBruteAi.class)
 public class PiglinBruteBrainMixin {
+    
+    @Inject( method = "getTargetIfWithinRange", at = @At("RETURN"), cancellable = true)
+    private static void getTargetIfWithinRange(AbstractPiglin piglinBrute, MemoryModuleType<? extends LivingEntity> memoryType, CallbackInfoReturnable<Optional<? extends LivingEntity>> cir) {
+		cir.setReturnValue(piglinBrute.getBrain().getMemory(memoryType).filter((livingEntity) -> {
+			if (livingEntity instanceof Player player) {
+				LivingEntity shape = PlayerShape.getCurrentShape(player);
 
-    /**
-     * @author ToCraft
-     *
-     * @reason method_30255 is the desugared lambda used by method_30249 that searches for a nearby player to aggro on.
-     * This mixin modifies the search logic to exclude players disguised as anything besides a Wither Skeleton or Wither.
-     */
-    @Inject( method = "method_30255", at = @At("HEAD"), expect = 0, cancellable = true)
-    private static void shape$method_30249FilterLambdaIntermediary(AbstractPiglin abstractPiglinEntity, LivingEntity livingEntity, CallbackInfoReturnable<Boolean> cir) {
-        if(livingEntity instanceof Player player) {
-            LivingEntity shape = PlayerShape.getCurrentShape(player);
+				if (shape != null) {
+					// Piglins should not attack Piglins or Piglin Brutes, unless they have
+					// hostility
+					if (shape.getType().is(WalkersEntityTags.PIGLIN_FRIENDLY)) {
+						return false;
+					}
 
-            if(shape != null && !(shape instanceof WitherSkeleton) && !(shape instanceof WitherBoss)) {
-                cir.setReturnValue(false);
-            }
-        }
-    }
+					// Player has a shape but is not a piglin, check config for what to do
+					else {
+						if (Walkers.CONFIG.hostilesIgnoreHostileShapedPlayer && shape instanceof Enemy) {
+
+							// Check hostility for aggro on non-piglin hostiles
+							if (PlayerHostility.hasHostility(player)) {
+								return false;
+							}
+						}
+					}
+				}
+			}
+			return livingEntity.closerThan(piglinBrute, 12.0);
+		}));
+	}
 }
