ad48dcd8c4d872fc9f485682eae1787dc105552f
ToCraft <piminecraft492@gmail.com>
2024-01-20 16:06:50 +0100

    magma cube & slime get jump boost 2

--- common/src/main/java/tocraft/walkers/api/WalkersTickHandlers.java
+++ common/src/main/java/tocraft/walkers/api/WalkersTickHandlers.java
@@ -21,6 +21,8 @@ public class WalkersTickHandlers {
         register(EntityType.RABBIT, new JumpBoostTickHandler<>(1));
         register(EntityType.GOAT, new JumpBoostTickHandler<>(2));
         register(EntityType.CAMEL, new JumpBoostTickHandler<>(0));
+        register(EntityType.MAGMA_CUBE, new JumpBoostTickHandler<>(2));
+        register(EntityType.SLIME, new JumpBoostTickHandler<>(2));
     }
 
     public static <T extends LivingEntity> void register(EntityType<T> type, WalkersTickHandler<T> handler) {
