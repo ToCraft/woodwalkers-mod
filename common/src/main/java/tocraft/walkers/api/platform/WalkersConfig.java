e0deb372d9261e961cf6cafabe972af93079d72c
ToCraft <piminecraft492@gmail.com>
2024-03-16 09:25:05 +0100

    add trader_llama to abilityCooldownMap

--- common/src/main/java/tocraft/walkers/api/platform/WalkersConfig.java
+++ common/src/main/java/tocraft/walkers/api/platform/WalkersConfig.java
@@ -62,6 +62,7 @@ public class WalkersConfig implements Config {
             put("minecraft:skeleton_horse", 40);
             put("minecraft:sniffer", 9600);
             put("minecraft:snow_golem", 10);
+            put("minecraft:trader_llama", 20);
             put("minecraft:warden", 200);
             put("minecraft:witch", 200);
             put("minecraft:wither", 200);
