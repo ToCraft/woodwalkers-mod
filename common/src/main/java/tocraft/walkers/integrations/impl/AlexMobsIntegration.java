fbfe7bf0f2712e4780e35a9459e723c8270460f5
To_Craft <development@tocraft.dev>
2024-08-18 13:07:47 +0200

    fix integration after testing

--- common/src/main/java/tocraft/walkers/integrations/impl/AlexMobsIntegration.java
+++ common/src/main/java/tocraft/walkers/integrations/impl/AlexMobsIntegration.java
@@ -2,21 +2,17 @@ package tocraft.walkers.integrations.impl;
 
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.monster.Creeper;
 import net.minecraft.world.entity.monster.Enemy;
-import net.minecraft.world.entity.player.Player;
 import tocraft.craftedcore.patched.Identifier;
 import tocraft.walkers.Walkers;
-import tocraft.walkers.api.PlayerShape;
 import tocraft.walkers.integrations.AbstractIntegration;
 import tocraft.walkers.traits.TraitRegistry;
 import tocraft.walkers.traits.impl.FearedTrait;
 import tocraft.walkers.traits.impl.PreyTrait;
 
 import java.util.List;
-import java.util.function.Predicate;
 
 @SuppressWarnings("unchecked")
 public class AlexMobsIntegration extends AbstractIntegration {
@@ -30,6 +26,7 @@ public class AlexMobsIntegration extends AbstractIntegration {
         registerAlexMobsTargetTagTrait("cachalot_whale");
         registerAlexMobsTargetTagTrait("crimson-mosquito");
         registerAlexMobsTargetTagTrait("crocodile");
+        TraitRegistry.registerByType(getType("fly"), (PreyTrait<Mob>) PreyTrait.ofHunterTag(getTag("fly_hurt_targets")));
         registerAlexMobsTargetTagTrait("froststalker");
         registerAlexMobsTargetTagTrait("giant-squid");
         registerAlexMobsTargetTagTrait("komodo-dragon");
@@ -39,18 +36,18 @@ public class AlexMobsIntegration extends AbstractIntegration {
         registerAlexMobsTargetTagTrait("sunbird_scorch");
         registerAlexMobsTargetTagTrait("tiger");
         registerAlexMobsTargetTagTrait("warped_toad");
-        TraitRegistry.registerByType(getType("bunfungus"), new PreyTrait<>(List.of(entity -> (entity instanceof Enemy && !(entity instanceof Creeper) && !(entity.getType().getCategory().getName().contains("water") && entity.isInWaterOrBubble()) && !entity.getType().is(getTag("bunfungus_ignores"))))));
+        TraitRegistry.registerByType(getType("bunfungus"), new PreyTrait<>(List.of(entity -> (entity instanceof Enemy && !(entity instanceof Creeper) && !(entity.getType().getCategory().name().contains("water") && entity.isInWaterOrBubble()) && !entity.getType().is(getTag("bunfungus_ignores"))))));
         // FearedTrait
         registerAlexMobsFearTagTrait("mimic_octopus");
         registerAlexMobsFearTagTrait("skunk");
     }
 
-    private void registerAlexMobsTargetTagTrait(String entityId) {
-        TraitRegistry.registerByType(getType(entityId), (PreyTrait<Mob>) PreyTrait.ofHunterTag(getTag(entityId + "anaconda_targets")));
+    private void registerAlexMobsTargetTagTrait(String entityId, int priority, int randInt) {
+        TraitRegistry.registerByTag(getTag(entityId + "_targets"), new PreyTrait<>(List.of(), List.of(getType(entityId)), List.of(), List.of(), priority, randInt));
     }
 
     private void registerAlexMobsFearTagTrait(String entityId) {
-        TraitRegistry.registerByType(getType(entityId), (FearedTrait<Mob>) FearedTrait.ofFearfulTag(getTag(entityId + "anaconda_targets")));
+        TraitRegistry.registerByTag((getTag(entityId + "_fears")), (FearedTrait<Mob>) FearedTrait.ofFearfulType(getType(entityId)));
     }
 
     private static EntityType<Mob> getType(String entityId) {
