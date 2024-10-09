1651671d261470147620c342533e636cb6147826
Shapovalov Viacheslav <slavko.ukrop@gmail.com>
2024-10-10 01:21:16 +0300

    add USE_SHAPE_ABILITY ShapeEvent

--- common/src/main/java/tocraft/walkers/api/events/ShapeEvents.java
+++ common/src/main/java/tocraft/walkers/api/events/ShapeEvents.java
@@ -3,15 +3,19 @@ package tocraft.walkers.api.events;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import org.jetbrains.annotations.Nullable;
 import tocraft.craftedcore.event.Event;
 import tocraft.craftedcore.event.EventFactory;
+import tocraft.walkers.ability.ShapeAbility;
 import tocraft.walkers.api.variant.ShapeType;
 
 public final class ShapeEvents {
     public static final Event<UnlockShapeCallback> UNLOCK_SHAPE = EventFactory.createWithInteractionResult();
 
     public static final Event<ShapeSwapCallback> SWAP_SHAPE = EventFactory.createWithInteractionResult();
+
+    public static final Event<UseShapeAbilityCallback> USE_SHAPE_ABILITY = EventFactory.createWithInteractionResult();
 
     @FunctionalInterface
     public interface ShapeSwapCallback {
@@ -21,5 +25,10 @@ public final class ShapeEvents {
     @FunctionalInterface
     public interface UnlockShapeCallback {
         InteractionResult unlock(ServerPlayer player, ShapeType<?> type);
+    }
+
+    @FunctionalInterface
+    public interface UseShapeAbilityCallback {
+        InteractionResult use(Player player, ShapeAbility<?> ability);
     }
 }
