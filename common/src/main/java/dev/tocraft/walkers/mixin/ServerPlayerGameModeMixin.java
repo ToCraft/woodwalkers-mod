b92520602b4b3a3aa174013517e3de15334e37a9
Elijah Stephenson <elijah.m.stephenson@gmail.com>
2025-09-26 14:26:44 -0500

    Now that issue is understood, cleaned up fix

--- common/src/main/java/dev/tocraft/walkers/mixin/ServerPlayerGameModeMixin.java
+++ common/src/main/java/dev/tocraft/walkers/mixin/ServerPlayerGameModeMixin.java
@@ -2,13 +2,8 @@ package dev.tocraft.walkers.mixin;
 
 import dev.tocraft.walkers.Walkers;
 import dev.tocraft.walkers.api.FlightHelper;
-import dev.tocraft.walkers.api.PlayerShape;
-import dev.tocraft.walkers.traits.TraitRegistry;
-import dev.tocraft.walkers.traits.impl.FlyingTrait;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.level.ServerPlayerGameMode;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.animal.FlyingAnimal;
 import net.minecraft.world.level.GameType;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -34,21 +29,13 @@ public class ServerPlayerGameModeMixin {
 
     @Inject(method = "setGameModeForPlayer", at = @At("RETURN"))
     public void onSetGameModeForPlayerReturn(GameType gameModeForPlayer, GameType previousGameModeForPlayer, CallbackInfo ci) {
-        // Handle flight permissions based on the NEW gamemode, not the old one
-        if (gameModeForPlayer.isSurvival()) {
-            // Check if player has flying permissions based on their current shape (ignoring creative mode)
-            LivingEntity shape = PlayerShape.getCurrentShape(this.player);
-            boolean hasShapeFlight = shape != null && Walkers.CONFIG.enableFlight
-                    && (TraitRegistry.has(shape, FlyingTrait.ID) || shape instanceof FlyingAnimal);
-            
-            if (hasShapeFlight) {
-                // Player has a flying shape, grant flight and restore flying state
-                FlightHelper.grantFlightTo(this.player);
-                this.player.getAbilities().flying = walkers$couldFly;
-            } else {
-                // Player doesn't have a flying shape, revoke flight
-                FlightHelper.revokeFlight(this.player);
-            }
+        // Use the improved hasFlyingPermissions method that checks the target gamemode
+        if (gameModeForPlayer.isSurvival() && Walkers.hasFlyingPermissions(this.player, gameModeForPlayer)) {
+            FlightHelper.grantFlightTo(this.player);
+            this.player.getAbilities().flying = walkers$couldFly;
+        } else if (gameModeForPlayer.isSurvival() && !Walkers.hasFlyingPermissions(this.player, gameModeForPlayer)) {
+            // If switching to survival and player doesn't have flying permissions, revoke flight
+            FlightHelper.revokeFlight(this.player);
         }
     }
 }
