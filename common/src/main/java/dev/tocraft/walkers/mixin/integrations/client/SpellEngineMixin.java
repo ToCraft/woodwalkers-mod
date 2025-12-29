ecde3072f48607bfe03b4762a2294bc87f431d0a
Karashok-Leo <1010177289@qq.com>
2025-12-29 17:05:39 +0800

    Fix first person issue with SpellEngine

---
+++ common/src/main/java/dev/tocraft/walkers/mixin/integrations/client/SpellEngineMixin.java
@@ -0,0 +1,31 @@
+package dev.tocraft.walkers.mixin.integrations.client;
+
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import dev.tocraft.walkers.api.PlayerShape;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.entity.player.Player;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+
+@Pseudo
+@Environment(EnvType.CLIENT)
+@Mixin(targets = "net.spell_engine.client.compatibility.FirstPersonModelCompatibility", remap = false)
+public class SpellEngineMixin
+{
+    @WrapMethod(
+        method = "isActive"
+    )
+    private static boolean wrap_isActive(Operation<Boolean> original)
+    {
+        Player player = Minecraft.getInstance().player;
+        if (player != null &&
+            PlayerShape.getCurrentShape(player) != null)
+        {
+            return true;
+        }
+        return original.call();
+    }
+}
