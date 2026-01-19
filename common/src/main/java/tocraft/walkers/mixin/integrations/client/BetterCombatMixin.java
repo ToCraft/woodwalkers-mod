f33eabbac16d5b27a24b60e3490b57607806a1db
Karashok-Leo <1010177289@qq.com>
2026-01-19 19:54:18 +0800

    Fix BetterCombatMixin
    (cherry picked from commit 8a13cb99c366cf52f3af7143757dca061754aa5e)

--- common/src/main/java/tocraft/walkers/mixin/integrations/client/BetterCombatMixin.java
+++ common/src/main/java/tocraft/walkers/mixin/integrations/client/BetterCombatMixin.java
@@ -1,27 +1,39 @@
 package tocraft.walkers.mixin.integrations.client;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import dev.tocraft.walkers.api.PlayerShape;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.Minecraft;
 import net.minecraft.world.entity.player.Player;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import tocraft.walkers.api.PlayerShape;
 
 @SuppressWarnings("UnresolvedMixinReference")
 @Pseudo
 @Environment(EnvType.CLIENT)
-@Mixin(targets = "net.bettercombat.compatibility.CompatibilityFlags", remap = false)
-public class BetterCombatMixin {
-    // fix Better Combat first person view in OLD Better Combat versions
-    @Inject(method = "firstPersonRender", at = @At("RETURN"), cancellable = true, require = 0)
-    private static void onFirstPersonRenderCall(CallbackInfoReturnable<Boolean> cir) {
+@Mixin(targets = "net.bettercombat.client.compat.FirstPersonAnimationCompatibility", remap = false)
+public class BetterCombatMixin
+{
+    @WrapOperation(
+        method = "firstPersonMode",
+        at = @At(
+            value = "FIELD",
+            target = "Lnet/bettercombat/client/compat/FirstPersonAnimationCompatibility;isCameraModPresent:Z",
+            opcode = Opcodes.GETSTATIC
+        ),
+        require = 0
+    )
+    private static boolean onFirstPersonRenderCall(Operation<Boolean> original)
+    {
         Player player = Minecraft.getInstance().player;
-        if (player != null && PlayerShape.getCurrentShape(player) != null) {
-            cir.setReturnValue(false);
+        if (player != null && PlayerShape.getCurrentShape(player) != null)
+        {
+            return true;
         }
+        return original.call();
     }
 }
